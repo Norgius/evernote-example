fc404940c5b4ae6046a714775264f6a543875e36
Norgius <dmitry.schadrin21@yandex.ru>
2022-11-17 22:52:42 +0300

    Some fixes

--- dump_inbox.py
+++ dump_inbox.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 import argparse
 
 from evernote.api.client import EvernoteClient
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 
 from config import Settings
 
-    
+
 def get_notebook_list(note_store, notebook_guid, number=10, offset=0):
     _filter = NoteStore.NoteFilter(notebookGuid=notebook_guid)
     resultSpec = NoteStore.NotesMetadataResultSpec(
@@ -25,11 +25,12 @@ def get_notebook_list(note_store, notebook_guid, number=10, offset=0):
     )
 
     # this determines which info you'll get for each note
-    return note_store.findNotesMetadata(_filter, offset, number, resultSpec);
+    return note_store.findNotesMetadata(_filter, offset, number, resultSpec)
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description=u'Dumps notes from Evernote inbox to console')
+    parser = argparse.ArgumentParser(
+        description=u'Dumps notes from Evernote inbox to console')
     parser.add_argument('number',
                         nargs='?',
                         type=int,
@@ -42,12 +43,14 @@ if __name__ == '__main__':
     client = EvernoteClient(token=config.EVERNOTE_PERSONAL_TOKEN)
     note_store = client.get_note_store()
 
-    notes = get_notebook_list(note_store, config.INBOX_NOTEBOOK_GUID, args.number).notes
+    notes = get_notebook_list(
+        note_store,
+        config.INBOX_NOTEBOOK_GUID, args.number
+    ).notes
 
-    # print('Notes', notes)
-    
     for counter, note in enumerate(notes, start=1):
         print('\n--------- %s ---------' % counter)
-        content = note_store.getNoteContent(note.guid)  # kwargs will be skipped by api because of bug
+        content = note_store.getNoteContent(note.guid)
+        print('NOTE_GUID: %s' % note.guid)
         soup = BeautifulSoup(content, "html.parser")
         print(soup.get_text())
